b08ff3486d027787bf424cea78f76ae337cc034c
Shellyaz26 <TheLittleCitizen4@gmail.com>
2020-10-01 13:28:57 +0300

    print picture to the screen

--- src/ClientComponent/Alldata.jsx
+++ src/ClientComponent/Alldata.jsx
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import "./Style.css";
 import NavBarSomeText from'./NavBarSomeText';
+import Meme from './printdata';
 
 
 export default function Data() {
     const [pictures, setPictures] = useState([
-        {name: 'a', description:"bla", path:'./pics/1.png'}
+        {name: 'Laught', description:"It's fun!", path:'./pics/1.png'},
+        {name: 'a', description:"bla", path:'./pics/2.png'}
     ]);
     const addPict = (newImage) => {
         setPictures([pictures, ...newImage]) ; 
@@ -13,10 +15,13 @@ export default function Data() {
     return (
         <div className="container">
             <div className="row">
-                <NavBarSomeText /> 
+                <NavBarSomeText 
+                addPict ={addPict}/> 
             </div>
             <div className="row">
-                <div className="col-md-3">
+                <div className="col-md-3"><br></br>
+                    <Meme 
+                    pictures={pictures}/>
             </div>
             <div className="col-md-3">
                 </div>
--- src/ClientComponent/printdata.jsx
+++ src/ClientComponent/printdata.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-export default function Mem({pictures}) {
+import "./Style.css";
+export default function Meme({pictures}) {
 
   return (
     <div >
-      <div className="App">
+      <div className="img">
         <h1>{pictures[0].name}</h1>
         <h1>{pictures[0].description}</h1>
         <img src={pictures[0].path} alt={pictures[0].altImg}/>
